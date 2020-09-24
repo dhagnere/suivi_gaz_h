e4d2094ac10617aeb5c7bc14af42c6c4645ec912
wothag <33983572+wothag@users.noreply.github.com>
2020-09-24 17:03:02 +0200

    MVC structuring
    Controller
    Model user created

--- app.js
+++ app.js
@@ -1,73 +1,29 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const listBatiments = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+//Router setup
+const router = require("./router");
 
 const app = express();
 
+//Setting views and view engine
 app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: true}));
+//setting up bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+//setting up the public folder
 app.use(express.static("public"));
 
-let batiments = [];
 
-app.get('/', (req, res, next) => {
-  res.render('home', {
-    homeStartingContent: homeStartingContent,
-    batiments: batiments
-  });
-});
+//use the router
+app.use('/', router);
 
-app.get('/listbatiments', (req , res , next) => {
-  res.render('listbatiments', {
-    listBatiments: listBatiments,
-    batiments: batiments
-  });
-});
-
-  app.get('/contact', (req, res, next) => {
-    res.render('contact', {
-      contactContent: contactContent
-    });
-  });
-
-  app.get('/create', (req, res, next) => {
-    res.render('create');
-  });
-
-  app.post('/create', (req, res, next) => {
-    const batiment = {
-      batiment: req.body.batiment,
-      pce: req.body.pce,
-      description: req.body.description,
-      checkboxInventaire: req.body.checkboxInventaire,
-      checkboxDetendeurs: req.body.checkboxDetendeurs,
-      checkboxDevis: req.body.checkboxDevis,
-      checkboxDevisValides: req.body.checkboxDevisValides,
-      checkboxEngagement: req.body.checkboxEngagement,
-      checkboxReglages: req.body.checkboxReglages,
-      checkboxAttestation: req.body.checkboxAttestation
-    };
-    batiments.push(batiment);
-    res.redirect('/listbatiments');
-  });
-
-
-
-
-
-
-
-
-
-
+//listen to port for server setup
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const validator = require('validator');
+
+let User = function (data) {
+  this.data = data
+  this.errors = []
+}
+
+User.prototype.validate = function () {
+  if (this.data.username == "") { this.errors.push("Vous devez fournir un nom d'utilisateur.") }
+  if (this.data.username !="" && !validator.isAlphanumeric(this.data.username)){this.errors.push("Le nom d'utiisateur doit être composé uniquement de caractères alphanumériques.")}
+  if (!validator.isEmail(this.data.email)) { this.errors.push("Vous devez fournir une adresse email valide.") }
+  if (this.data.password == "") { this.errors.push("Vous ne pouvez laisser un mot de passe vide.") }
+  if (this.data.password.length > 0 && this.data.password.length < 12) { this.errors.push("Le mot de passe doit être composé de 12 caractères minimum") }
+  if (this.data.password.length > 100) { this.errors.push("Le mot de passe ne peux pas dépasser 100 caractères") }
+  if (this.data.username.length > 0 && this.data.username.length < 3) { this.errors.push("Le nom être composé de 3  caractères minimum") }
+  if (this.data.username.length > 30) { this.errors.push("Le nom d'utilisateur ne peux pas dépasser 30 caractères") }
+  
+  
+  }
+
+
+User.prototype.register = function () {
+  //step 1 : validate user date
+  this.validate()
+  //setp 2 : only if there is no validation errors
+  //then save the user data into database
+}
+
+module.exports = User;
